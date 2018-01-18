4e8258defa263f561114c299b778683275834110
a998151 <yang998151@163.com>
2018-01-18 22:38:52 +0800

    删除了两个变量

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import Flower.Rose;
 
 public class Main {
     private static final int VALUE_ONE=100;
-    private final int count1=99;
-    private int count2=98;
     public static void main(String[] args) {
 
         System.out.println(VALUE_ONE);
